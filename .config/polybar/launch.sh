2c5c7cfa6358e12e58c5084025ed9a3c7b4ad9ed
starkbaum <starkbaum.stefan@gmail.com>
2026-03-28 02:56:53 +0100

    Added polybar

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Kill any running polybar instances
+killall -q polybar
+
+# Wait until processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch polybar
+polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched..."
