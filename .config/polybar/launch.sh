ede7253cfd47e58e6b70b13a1beb4ee02988b1d0
starkbaum <starkbaum.stefan@gmail.com>
2026-03-28 07:56:33 +0100

    Fixed floating polybar using transparent borders as found in https://www.reddit.com/r/unixporn/comments/80qi40/comment/duxziw1/\?utm_source\=reddit-android

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 # Kill any running polybar instances
 killall -q polybar
-
-# Wait until processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch polybar
-polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+# Get screen width and calculate polybar width matching i3 gaps
+SCREEN_WIDTH=$(xrandr | grep '\*' | awk '{print $1}' | cut -d'x' -f1 | head -1)
+GAPS=15  # gaps outer (5) + gaps inner (10)
+BAR_WIDTH=$((SCREEN_WIDTH - (2 * GAPS)))
+OFFSET_X=$GAPS
 
-echo "Polybar launched..."
+# Export for polybar
+export BAR_WIDTH
+export OFFSET_X
+
+# Launch polybar
+BAR_WIDTH=$BAR_WIDTH OFFSET_X=$OFFSET_X polybar main 2>&1 | tee -a /tmp/polybar.log & disown
+
+echo "Polybar launched... (width: $BAR_WIDTH, offset: $OFFSET_X)"
